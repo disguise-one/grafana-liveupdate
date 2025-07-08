95dea2f3ee171a9ff9e4cbc9154f52a6888f89fb
Peter Kirkup <peter.kirkup@disguise.one>
2025-07-08 14:22:37 +0100

    fix(ci): Add cleanup to start script to prevent container conflicts

--- start-grafana.bat
+++ start-grafana.bat
@@ -4,6 +4,11 @@ TITLE Grafana LiveUpdate Control
 ECHO Starting Grafana Plugin Advertiser in a new window...
 START "Plugin Advertiser" .\\advertiser\\plugin_advertiser.exe
 
+ECHO.
+ECHO Cleaning up old containers (if any)...
+docker compose down
+
+ECHO.
 ECHO Starting Docker containers...
 ECHO Press Ctrl+C in this window to stop Grafana and the advertiser.
 docker compose up
@@ -11,7 +16,7 @@ docker compose up
 :: This part of the script will run after `docker compose up` is stopped.
 ECHO.
 ECHO Grafana containers stopped. Stopping the advertiser process...
-taskkill /IM plugin_advertiser.exe /F
+taskkill /IM plugin_advertiser.exe /F /FI "STATUS eq RUNNING" >nul 2>&1
 
 ECHO.
 ECHO All services stopped.
